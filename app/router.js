8793b088d8edc4a8a31b6f3ef1e3cde5420a247f
fabischapeli <fabischapeli97@gmail.com>
2017-11-18 09:50:08 +0300

    dynamic routing and finding records

--- app/router.js
+++ app/router.js
@@ -1,14 +1,14 @@
-import EmberRouter from '@ember/routing/router';
+import Ember from 'ember';
 import config from './config/environment';
 
-const Router = EmberRouter.extend({
-  location: config.locationType,
-  rootURL: config.rootURL
+const Router = Ember.Router.extend({
+  location: config.locationType
 });
 
 Router.map(function() {
   this.route('about');
   this.route('contact');
+  this.route('rental', {path: '/rental/:rental_id'});
 });
 
 export default Router;
--- app/routes/index.js
+++ app/routes/index.js
@@ -35,21 +35,6 @@ export default Ember.Route.extend({
      var newRental = this.store.createRecord('rental', params);
      newRental.save();
      this.transitionTo('index');
-   },
-
-   update(rental, params) {
-    Object.keys(params).forEach(function(key) {
-      if(params[key]!==undefined) {
-        rental.set(key,params[key]);
-      }
-    });
-    rental.save();
-    this.transitionTo('index');
-   },
-
-    destroyRental(rental) {
-      rental.destroyRecord();
-      this.transitionTo('index');
-    }
+   }
   }
 });
