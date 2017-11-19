0b68aae2e112db52181bc2c01f9440c5821e9fd0
fabischapeli <fabischapeli97@gmail.com>
2017-11-19 11:54:16 +0300

    one-route with multiple models

--- app/routes/index.js
+++ app/routes/index.js
@@ -27,13 +27,21 @@ var rentals = [{
 
 export default Ember.Route.extend({
   model() {
-    return this.store.findAll('rental');
+    return Ember.RSVP.hash({
+      rentals: this.store.findAll('rental'),
+      reviews: this.store.findAll('review')
+    });
   },
 
   actions: {
     saveRental3(params) {
      var newRental = this.store.createRecord('rental', params);
      newRental.save();
+     this.transitionTo('index');
+   },
+   saveReview(params) {
+     var newReview = this.store.createRecord('review', params);
+     newReview.save();
      this.transitionTo('index');
    }
   }
